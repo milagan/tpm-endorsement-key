2b8e36c302c38c400200d5955f68b8fc41ca627a
Maurice Ilagan <maurice.ilagan@checkpt.com>
2019-03-17 17:10:45 -0400

    Add initial code.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.Devices.Provisioning.Security;
+using Microsoft.Azure.Devices.Shared;
+
+namespace tpm_endorsement_key
+{
+    class Program
+    {
+        private const string RegistrationId = "testtpmregistration1";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Extracting endorsement key...");
+            var tpmProvider = new SecurityProviderTpmHsm(RegistrationId);
+            string base64EK = Convert.ToBase64String(tpmProvider.GetEndorsementKey());
+
+            Console.WriteLine(base64EK);
+        }
+    }
+}
